82dbb1148e08e330d63055b4be6b1baefa5edcf3
unknown <rmtestard@gmail.com>
2020-04-04 17:41:43 +0200

    I have added a loop showing our selected videos

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,47 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="videos"></VideoList>
   </div>
 </template>
 
 <script>
-import SearchBar from './components/SearchBar'
+import axios from 'axios';
+
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 
 export default {
   name: 'App',
   components: {
-    SearchBar
+    SearchBar,
+    VideoList,
+  },
+  data() {
+    return { videos: [] };
   },
   methods: {
-    onTermChange: (searchTerm) => {
-      console.log(searchTerm)
-    }
-  }
-}
+    async onTermChange(searchTerm) {
+      try {
+        const result = await axios.get(
+          'https://www.googleapis.com/youtube/v3/search',
+          {
+            params: {
+              key: process.env.VUE_APP_API_KEY,
+              type: 'video',
+              part: 'snippet',
+              q: searchTerm,
+            },
+          }
+        );
+        this.videos = result.data.items;
+        console.log(this.videos);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
 
 <style>
